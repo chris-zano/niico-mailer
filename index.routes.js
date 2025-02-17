6cdcc16e7fde17a1fd338c0d97580222f1e94b94
Christian Solomon <cncs.chris@gmail.com>
2025-02-17 12:45:23 +0000

    fix cors

--- index.routes.js
+++ index.routes.js
@@ -13,10 +13,6 @@ export const routes = (app) => {
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-        if (req.method === "OPTIONS") {
-            return res.sendStatus(204); // No Content
-        }
-
         next();
     });
 
