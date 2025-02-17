7deaa708c1df19fbb0518cdebbcada01a5c8edaa
Christian Solomon <cncs.chris@gmail.com>
2025-02-17 12:37:21 +0000

    update cors settings

--- index.routes.js
+++ index.routes.js
@@ -1,12 +1,22 @@
 
 import authRoutes from './routes/auth/auth.routes.js';
+import cors from 'cors';
 
 export const routes = (app) => {
-    app.use((req, res, next) => {
-        console.log(`Request received: ${req.method} ${req.url}`);
 
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // app.use(cors());
+
+    app.use((req, res, next) => {
+        console.log("Request received:", req.method, req.url);
+
+        res.header("Access-Control-Allow-Origin", "*"); // Change "*" to your frontend URL if needed
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(204); // No Content
+        }
+
         next();
     });
 
